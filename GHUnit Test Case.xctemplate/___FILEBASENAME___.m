b7eb1e47f9d3495eabce4af8d0587db99288fc7b
vincent daubry <vincentdaubry@MacBook-Pro-de-Vincent.local>
2013-07-11 14:59:05 +0200

    ajout template GHUnit

---
+++ GHUnit Test Case.xctemplate/___FILEBASENAME___.m
@@ -0,0 +1,62 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
+//
+
+#import <GHUnitIOS/GHUnit.h>
+//#import <OCMock/OCMock.h>
+ 
+@interface ___FILEBASENAMEASIDENTIFIER___ : GHTestCase { }
+@end
+ 
+@implementation ___FILEBASENAMEASIDENTIFIER___
+
+/*****************************************************************************
+ *
+ *
+ *  Setup Methods
+ *
+ *****************************************************************************/
+
+#pragma mark - Setup Methods
+
+
+- (void)setUpClass
+{
+    // Run at start of all tests in the class
+}
+
+- (void)tearDownClass
+{
+    // Run at end of all tests in the class
+}
+
+- (void)setUp
+{
+    // Run before each test method
+}
+
+- (void)tearDown
+{
+    // Run after each test method
+}
+
+/*****************************************************************************
+ *
+ *
+ *  Test Methods
+ *
+ *****************************************************************************/
+
+#pragma mark - Test Methods
+
+- (void)test_<#method name#>_<#state under test#>_should_<#expectation#>
+{
+    GHAssertTrue(NO, @"implement the test");
+}
+
+ 
+@end
--- README.md
+++ README.md
@@ -2,3 +2,13 @@ Xcode-Templates
 ===============
 
 XCode templates (GHUnit, ViewControllers, etc)
+
+
+## Installation instructions
+
+drop the .xctemplate bundle to ~/Library/Developer/XCode/Templates/File Templates/GHUnit/ 
+You need to create the GHUnit folder and it will appear as a new group in the new file template window in XCode.
+
+## License 
+
+This template is available under the MIT license. See the license file for more details.
